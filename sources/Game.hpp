292594ae6d9c17460df762f58d717f72d1479c36
Noah Weiss <93292627+noahweiss890@users.noreply.github.com>
2022-05-29 02:07:44 +0300

    Update Game.hpp

--- sources/Game.hpp
+++ sources/Game.hpp
@@ -8,4 +8,11 @@ class Game {
     Team away;
     int home_score;
     int away_score;
+    public:
+        Game(Team &home, Team &away) {
+            this->home = home;
+            this->away = away;
+            this->home_score = 55 + (rand() % 46) + home.talent;
+            this->away_score = 50 + (rand() % 51) + away.talent;
+        }
 };
