2ea6c6d7fb14e90f74efa69c622012daad36d3f9
Noah Weiss <93292627+noahweiss890@users.noreply.github.com>
2022-05-29 02:08:21 +0300

    Update Team.hpp

--- sources/Team.hpp
+++ sources/Team.hpp
@@ -7,6 +7,9 @@ class Team {
         string name;
         int talent;
     public:
+        Team() {
+            
+        }
         Team(string name, int talent) {
             this->name = name;
             this->talent = talent;
