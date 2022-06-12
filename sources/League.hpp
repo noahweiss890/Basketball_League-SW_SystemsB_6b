de670951003951db707c814e75d85bec3d257dfe
Noah Weiss <93292627+noahweiss890@users.noreply.github.com>
2022-06-13 02:49:07 +0300

    Update League.hpp

--- sources/League.hpp
+++ sources/League.hpp
@@ -5,8 +5,8 @@
 #include <random>
 #include "Team.hpp"
 
-#define LEAGUE_SIZE 20
-#define NBA_SIZE 30
+constexpr int LEAGUE_SIZE = 20;
+constexpr int NBA_SIZE = 30;
 
 using namespace std;
 
@@ -16,18 +16,18 @@ class League {
         League() {
             league_constructor(LEAGUE_SIZE);
         }
-        League(vector<Team*> teams) {
-            if(teams.size() > 20) {
+        League(vector<Team*> const &teams) {
+            if(teams.size() > LEAGUE_SIZE) {
                 throw invalid_argument("too many teams for the league");
             }
             for(Team* team : teams) {
                 this->teams.push_back(new Team(team->get_name(), team->get_talent()));
             }
             // fill the rest in with NBA team names
-            league_constructor(LEAGUE_SIZE - teams.size());
+            league_constructor(LEAGUE_SIZE - (int)teams.size());
         }
         vector<Team*> get_teams();
-        void set_teams(vector<Team*> teams);
+        void set_teams(vector<Team*> const &teams);
     private:
         void league_constructor(int amount);
 };
