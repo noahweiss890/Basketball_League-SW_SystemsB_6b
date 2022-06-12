5c1cfba46df615c8d575f8fa3749a3cccd347e1a
Noah Weiss <93292627+noahweiss890@users.noreply.github.com>
2022-06-13 02:49:37 +0300

    Update Schedule.hpp

--- sources/Schedule.hpp
+++ sources/Schedule.hpp
@@ -8,14 +8,16 @@
 #include "Game.hpp"
 #include "League.hpp"
 
-#define LEAGUE_SIZE 20
-
 using namespace std;
 
 class Schedule {
     unordered_map<int, vector<Game*>> rounds;
     vector<Team> rankings;
     public:
+        Schedule() {
+            League leag;
+            Schedule sc(leag);
+        }
         Schedule(League lea) {
             if(lea.get_teams().size() != LEAGUE_SIZE) {
                 throw invalid_argument("league must have exactly 20 teams");
@@ -29,17 +31,56 @@ class Schedule {
                     this->rounds[GAMES_AMOUNT + 1 - i].push_back(new Game(lea.get_teams().at(LEAGUE_SIZE - 1 - k), lea.get_teams().at(k)));
                 }
                 vector<Team*> temp = lea.get_teams();
-                rotate(temp.begin() + 1, temp.begin() + 20 - 1, temp.end());
+                rotate(temp.begin() + 1, temp.begin() + LEAGUE_SIZE - 1, temp.end());
                 lea.set_teams(temp);
             }
-            for(auto team : lea.get_teams()) {
+            for(auto* team : lea.get_teams()) {
                 this->rankings.push_back(*team);
             }
             std::sort(this->rankings.begin(), this->rankings.end(), std::greater<Team>());
         }
-
+        Schedule(const Schedule &other) {
+            this->rankings = other.rankings;
+            for(auto const &round : other.rounds) {
+                this->rounds[round.first] = round.second;
+            }
+        }
+        Schedule& operator=(const Schedule& sc) {
+            if(this != &sc) {
+                this->rankings = sc.rankings;
+                for(auto const &round : sc.rounds) {
+                    this->rounds[round.first] = round.second;
+                }
+            }
+            return *this;
+        }
+        Schedule(Schedule&& other) noexcept {
+            this->rankings = other.rankings;
+            for(auto const &round : other.rounds) {
+                this->rounds[round.first] = round.second;
+            }
+            other.rankings.clear();
+            for(auto round : other.rounds) {
+                round.second.clear();
+            }
+            other.rounds.clear();
+        }
+        Schedule& operator=(Schedule&& other) noexcept {
+            if (this != &other) {
+                this->rankings = other.rankings;
+                for(auto const &round : other.rounds) {
+                    this->rounds[round.first] = round.second;
+                }
+                other.rankings.clear();
+                for(auto round : other.rounds) {
+                    round.second.clear();
+                }
+                other.rounds.clear();
+            }
+            return *this;
+        }
         ~Schedule() {
-            for(auto rou : this->rounds) {
+            for(auto const &rou : this->rounds) {
                 for(size_t k = 0; k < this->rounds[rou.first].size(); k++) {
                     delete this->rounds[rou.first].at(k);
                 }
